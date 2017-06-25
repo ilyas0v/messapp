45dd852dbc595d9068bfd6aa9cdef4e0148705b1
Ilyas Ilyasov <Ilyas Ilyasov>
2017-06-25 06:22:10 +0400

    added mongodb

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var MongoClient = require('mongodb').MongoClient;
+
+
+
+var url = "mongodb://ilyas0v:54123ii@ds137882.mlab.com:37882/messapp";
+
+
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-/*
-app.post('/login', function(request, response){
-    console.log(request.body.username); 
-    console.log(request.body.pass);
-});*/
+app.get('/m',function(req,res){
+	
+	var mesajlar="a";
+	var url = "mongodb://ilyas0v:54123ii@ds137882.mlab.com:37882/messapp";
+	MongoClient.connect(url, function(err, db) {
+	  if (err) {console.log("cant connect to database"); return;}
+	  var query = {};
+	  db.collection("mesajlar").find(query).toArray(function(err, result) {
+		if (err) throw err;
+		console.log(result);
+		mesajlar = result;
+		res.send(mesajlar);
+		db.close();
+	  });
+	});
+	
+	
+});
+
+
 
 
 io.on('connection', function(socket){
 	socket.on('chat message', function(msg){
+		if(msg!='' && msg!=' '){
+		MongoClient.connect(url, function(err, db) {
+			if (err) throw err;
+			var myobj = { gonderen: msg[msg.length-1], mesaj: msg };
+			db.collection("mesajlar").insertOne(myobj, function(err, res) {
+				if (err) throw err;
+				console.log("1 record inserted");
+				db.close();
+			});
+		});
+		
+		}
+		
 		io.emit('chat message', msg);
 	});
 });
