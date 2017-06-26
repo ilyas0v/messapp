b6506ff3d664b2ffc0d19e6e7a393f5abdb12d5b
Ilyas Ilyasov <Ilyas Ilyasov>
2017-06-26 06:52:57 +0400

    added login

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var MongoClient = require('mongodb').MongoClient;
 
+var bodyParser = require("body-parser");
 
 
 var url = "mongodb://ilyas0v:54123ii@ds137882.mlab.com:37882/messapp";
@@ -10,11 +11,36 @@ var url = "mongodb://ilyas0v:54123ii@ds137882.mlab.com:37882/messapp";
 
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile('login.html');
 });
 
+
+
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.post("/", function(request, response) {
+  var un = request.body.username;
+  var pass = request.body.pass;
+  
+  var istifadeci_say = 0;
+  
+  MongoClient.connect(url, function(err, db) {
+	  if (err) {console.log("cant connect to database"); return;}
+	  var query = {ad:un,pass:pass};
+	  db.collection("istifadeciler").find(query).toArray(function(err, result) {
+		if (err) throw err;
+		console.log(result);
+		if(result.length!=0){response.sendfile('index.html'); }
+		else{response.end('Girish melumatlari yanlishdir..');}
+		db.close();
+	  });
+	});
+  
+});
+
+
 app.get('/m',function(req,res){
-	
 	var mesajlar="a";
 	var url = "mongodb://ilyas0v:54123ii@ds137882.mlab.com:37882/messapp";
 	MongoClient.connect(url, function(err, db) {
@@ -28,8 +54,6 @@ app.get('/m',function(req,res){
 		db.close();
 	  });
 	});
-	
-	
 });
 
 
